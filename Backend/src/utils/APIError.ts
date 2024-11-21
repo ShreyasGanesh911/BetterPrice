304cfec43e3244485c23af961cc31a76ea30b7f3
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-11-21 13:28:11 +0530

    added utils

--- Backend/src/utils/APIError.ts
+++ Backend/src/utils/APIError.ts
@@ -1,12 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 type Error = {
     message:string,
-    status:number
+    statusCode:number
 }
 const APIError = (err:Error,req:Request,res:Response,next:NextFunction)=>{
-    const status = err.status || 500;
+    const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
-    res.status(status).json({
+    res.status(statusCode).json({
         success:false,
         message
     })
