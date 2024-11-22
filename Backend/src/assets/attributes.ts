04648b70e644a1cf261a46c82755529050d813ce
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-11-22 19:33:32 +0530

    added class attributes

---
+++ Backend/src/assets/attributes.ts
@@ -0,0 +1,13 @@
+export const flipkartAttributes = {
+    title: "div.KzDlHZ",
+    price: "Nx9bqj _4b5DiR",
+    link: "a.CGtC98",
+    rev:"span.a-size-base.s-underline-text"
+}
+
+export const amazonAttributes = {
+    title: "span.a-size-medium.a-color-base.a-text-normal",
+    price: "a-price-whole",
+    link:"a.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal",
+    rev:"span.a-size-base.s-underline-text"
+}
