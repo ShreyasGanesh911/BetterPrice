d6369d30f5c386c06e3d8f6367922f798d4f87f4
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-11-23 19:37:31 +0530

    added cors

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import APIError from './utils/APIError.js'
 import digitalRouter from './Routes/Digital.js'
+import cors from "cors"
+
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use("/e",digitalRouter)
-
 app.use(APIError)
 
 export default app
