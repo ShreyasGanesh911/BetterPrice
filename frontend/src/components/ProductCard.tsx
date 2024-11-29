f9a84a78faf3cd62bbcda21f1ac64d881b07c187
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-11-29 13:40:56 +0530

    added image component

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
-export default function ProductCard({link,title,price}:{link:string,title:string,price:string}) {
+export default function ProductCard({link,title,price,img}:{link:string,title:string,price:string,img: string}) {
   return (
-    <div className='w-full border border-black h-32 flex my-2 text-white hover:border-white hover'>
+      <a target='_blank' href={link} rel="noreferrer">
+            <div className='w-full border border-black h-32 flex my-2 text-white hover:border-white hover'>
         <div className='w-3/4 bg-black h-full flex flex-col justify-center pl-3'>
             <p className='truncate '>{title}</p>
-            <p>₹{price}/-</p>
+            <p>₹{price?price:null}/-</p>
             <p><a target='_blank' href={link} rel="noreferrer">View Product</a></p>
         </div>
-        <div className='w-1/4 bg-purple-500 h-full'></div>
+        <div className='w-1/4 bg-black h-full '>
+          <img className='object-contain w-full h-full' src={img} alt="" />
+        </div>
     </div>
+      </a>
   )
 }
